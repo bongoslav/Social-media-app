29eacd0b99738bfc9940c29c592d024de2dd6460
bongoslav <beborisov99@gmail.com>
2023-04-17 23:00:56 +0300

    added the remaining registration tests

--- src/__tests__/auth.test.ts
+++ src/__tests__/auth.test.ts
@@ -1,39 +1,95 @@
 import request from "supertest";
 import server from "../../index";
+import User from "../models/User";
 
-afterAll((done) => {
-  server.close(done);
+const validRegisterData = {
+  username: "validUser",
+  email: "validuser@example.com",
+  password: "validPassword",
+  confirmPassword: "validPassword",
+};
+
+const invalidRegisterData = {
+  username: "",
+  email: "invalidEmail",
+  password: "dsa",
+  confirmPassword: "asd",
+};
+
+const invalidRegisterDataResponse = [
+  {
+    value: "",
+    msg: "Username is required",
+    param: "username",
+    location: "body",
+  },
+  {
+    value: "",
+    msg: "Username should be at least 6 characters long",
+    param: "username",
+    location: "body",
+  },
+  {
+    value: "invalidEmail",
+    msg: "Invalid email address",
+    param: "email",
+    location: "body",
+  },
+  {
+    value: "dsa",
+    msg: "Password should be at least 6 characters long",
+    param: "password",
+    location: "body",
+  },
+  {
+    value: "asd",
+    msg: "Passwords do not match",
+    param: "confirmPassword",
+    location: "body",
+  },
+];
+
+afterAll(async () => {
+  await server.close();
+  await User.deleteOne({ username: "validUser" });
 });
 
-describe("auth", () => {
-  describe("user registration", () => {
-    describe("given all is valid", () => {
-      test("should return true if all is fine", async () => {
-        const response = await request(server).post("/auth/register").send({
-          username: "validUser",
-          email: "validuser@example.com",
-          password: "validPassword",
-          confirmPassword: "validPassword",
-        });
-        expect(response.status).toEqual(201);
-      });
+describe("user registration", () => {
+  describe("given all is valid", () => {
+    test("should return true if all is fine", async () => {
+      const response = await request(server)
+        .post("/auth/register")
+        .send(validRegisterData);
+      expect(response.status).toEqual(201);
     });
-    describe("given there is validation fail", () => {
-      // TODO:
-      // test("should return status code 422 and the errors messages", async () => {});
+  });
+  describe("given there is validation fail", () => {
+    test("should return status code 422 and the errors messages", async () => {
+      const response = await request(server)
+        .post("/auth/register")
+        .send(invalidRegisterData);
+      expect(response.status).toEqual(422);
+      expect(response.body).toEqual(invalidRegisterDataResponse);
     });
-    describe("verify that user does not already exist", () => {});
-    describe("the user object gets returned", () => {});
   });
-
-  describe("user login", () => {
-    // ...
-    // ...
-    // creating a user session (cookie?)
+  describe("verify that user does not already exist", () => {
+    test("should return status code 400 and the error message", async () => {
+      const response = await request(server)
+        .post("/auth/register")
+        .send(validRegisterData);
+      expect(response.status).toEqual(400);
+      expect(response.body.error).toEqual("User already exists");
+    });
   });
-
-  describe("user logout", () => {});
 });
+
+describe("user login", () => {
+  // ...
+  // ...
+  // creating a user session (cookie?)
+});
+
+describe("user logout", () => {});
 
 /*
  describe("user login", () => {
