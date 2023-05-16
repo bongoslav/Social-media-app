d77a7d9e1458902935099154c88a1996638acf02
bongoslav <beborisov99@gmail.com>
2023-05-17 01:45:35 +0300

    removed redundant code

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -19,20 +19,8 @@ export function Home() {
     fetchData();
   }, []);
 
-  // in order to display the current user's info
-  // i need to create an endpoint in the backend
-  // see here https://github.com/KartikSingh023/Sociopedia/blob/main/client/src/scenes/widgets/UserWidget.jsx
-  // `getUser`
-  // (getUser controller with a route) and
-  // const user = await User.findById(id);
-  // res.status(200).json(user);))
-  // however password?
 
   return (
-    // TODO:
-    // display # of likes and comments
-    // add like/comments
-    // improve UI
     <div>
       {posts.map((post) => (
         <Post key={post._id} {...post} />
